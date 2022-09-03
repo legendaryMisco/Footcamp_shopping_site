0c4fd80ecba9af73608a8377b1ceab85dc11d841
legendaryMisco <91750453+legendaryMisco@users.noreply.github.com>
2022-09-03 01:55:34 +0100

    Delete sneakers directory

--- sneakers/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class SneakersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sneakers'
-
-    def ready(self):
-        import sneakers.signals
